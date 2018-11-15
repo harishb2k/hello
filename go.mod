1f22596bf46a31c139d1aefdaf7915a8e87cdf37
Harish Singh Bohara <harish.bohara@olacabs.com>
2018-11-15 13:39:14 +0530

    First commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/harishb2k/hello
+
+require rsc.io/quote v1.5.2
---
+++ hello.go
@@ -0,0 +1,10 @@
+package hello
+
+import (
+    "fmt"
+    "rsc.io/quote"
+)
+
+func SayHi() {
+    fmt.Println(quote.Hello())
+}
