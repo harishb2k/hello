d9225c23111852b360ffb98d6dd1fcebbe35153b
Harish Singh Bohara <harish.bohara@olacabs.com>
2018-11-15 13:51:52 +0530

    Added V2

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/harishb2k/hello
+module github.com/harishb2k/hello/v2
 
 require rsc.io/quote v1.5.2
--- hello.go
+++ hello.go
@@ -7,5 +7,6 @@ import (
 
 func SayHi() {
     fmt.Println("This is change 1 on version 0")
+    fmt.Println("This is change 2.1 on version 2")
     fmt.Println(quote.Hello())
 }
